0327028ed301e155ffd1b9d248dc45adb1cce2b7
Gabriel Santos <gabmaxsan@gmail.com>
2019-01-17 18:50:34 -0200

    add image model

---
+++ src/app/model/Image.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Infoalto\Admin;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Image extends Model
+{
+    protected $fillable = ['name','directory', 'height', 'width', 'title', 'imageable_type', 'imageable_id'];
+
+    public function imageable(){
+        return $this->morphTo();
+    }
+}
