1f7e3b1e21ca05086dd0a66c7008199cb062b59b
Gabriel Santos <gabmaxsan@gmail.com>
2019-01-17 15:09:06 -0200

    fixed message icon

--- src/resources/views/layout/messages.blade.php
+++ src/resources/views/layout/messages.blade.php
@@ -8,5 +8,12 @@
 @endif --}}
 <div class="alert alert-{{$type}} alert-dismissible" role="alert">
     <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-    <i class="fa fa-times-circle"></i> {{ $slot }}
+    @if($type == "success")
+        <i class="fa fa-check"></i>
+    @elseif($type == "error")
+        <i class="fa fa-times"></i>
+    @else
+        <i class="fa fa-{{$type}}"></i>
+    @endif
+    {{ $slot }}
 </div>
