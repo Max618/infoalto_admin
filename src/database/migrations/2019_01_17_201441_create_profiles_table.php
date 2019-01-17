2b3bd4e9c678ca93c4c86f4be35a5012640e6bf7
Gabriel Santos <gabmaxsan@gmail.com>
2019-01-17 18:21:42 -0200

    add profile migration

---
+++ src/database/migrations/2019_01_17_201441_create_profiles_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProfilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->date("birhday");
+            $table->number("phone",11);
+            $table->number("cpf",11)->nullable();
+            $table->string("rg",9)->nullable();
+            $table->text("about")->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profiles');
+    }
+}
