dba0ba043134dbedba0e0217d9f28b5cdcebe46d
Gabriel Santos <gabmaxsan@gmail.com>
2019-01-17 17:00:44 -0200

    add edit role view

--- src/resources/views/admin/role/edit.blade.php
+++ src/resources/views/admin/role/edit.blade.php
@@ -1,1 +1,27 @@
-<p>EDIT ROLE</p>
+@extends('layout.dashboard')
+
+@section('content')
+<h3 class="page-title">Editar Função</h3>
+<div class="row">
+    <div class="col-md-12">
+        <div class="panel">
+            <div class="panel-body">
+            <!-- FORM HOVER -->
+            {!! Form::model($role, ["route" => ["role.update", $role], "method" => "put", "class" => "panel-body"]) !!}
+                {!! Form::text("name",null, ["class" => "form-control", "placeholder" => "Digite o Nome"]) !!}
+                <br>
+                {!! Form::textarea("description",null, ["class" => "form-control", "placeholder" => "Digite a Descrição"]) !!}
+                <br>
+                {!! Form::text("permissions",null, ["class" => "form-control", "placeholder" => "Digite as permissões separdas por / usando o padrao (create,edit,view,delete) e a model"]) !!}
+                <br>
+                <div class="col-md-12 text-center">
+                    {!! Form::submit("Salvar", ["class" => "btn btn-primary"]) !!}
+                    <a class="btn btn-danger" href="{{ route('role.index') }}">Cancelar</a>
+                </div>
+            {!! Form::close() !!}
+            <!-- END FORM HOVER -->
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
