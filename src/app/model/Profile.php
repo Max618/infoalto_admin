1bd5858078537c6fce9450c2cdd02312cdeee46b
Gabriel Santos <gabmaxsan@gmail.com>
2019-01-17 20:41:18 -0200

    add mutators for birthday and phone

--- src/app/model/Profile.php
+++ src/app/model/Profile.php
@@ -3,6 +3,7 @@
 namespace Infoalto\Admin;
 
 use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
 
 class Profile extends Model
 {
@@ -11,6 +12,18 @@ class Profile extends Model
     protected $casts = [
         'birthday' => 'date:Y-m-d'
     ];
+
+    public function getBirthdayAttribute(){
+        return (new Carbon($this->attributes["birthday"]))->format("d/m/Y");
+    }
+
+    public function getPhoneAttribute(){
+        $novo = substr_replace($this->attributes["phone"], '(', 0, 0);
+        $novo = substr_replace($novo, ') ', 3, 0);
+        $novo = substr_replace($novo, " ", 6, 0);
+        $novo = substr_replace($novo, "-", 11,0);
+        return $novo;
+    }
 
     public function user(){
         return $this->hasOne("App\User");
