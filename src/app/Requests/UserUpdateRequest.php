48f826c9de6bafe8edd8c7992ff49cf0322402e6
Gabriel Santos <gabmaxsan@gmail.com>
2019-01-17 14:20:51 -0200

    add user update request

---
+++ src/app/Requests/UserUpdateRequest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Infoalto\Admin\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use App\User;
+
+class UserUpdateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return $this->user()->can('edit_user');
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            "name" => "required|max:255",
+            "email" => "required||max:255",
+            "role_id" => "required|numeric"
+        ];
+    }
+
+    public function messages(){
+        return [
+            "name.required" => "O campo é nome é obrigatório",
+            "name.max" => "O campo nome passou do limite máximo de caracteres",
+            "email.required" => "O campo é e-mail é obrigatório",
+            "email.max" => "O campo e-mail passou do limite máximo de caracteres",
+            "role_id.required" => "O campo função é ogrigatório",
+        ];
+    }
+}
