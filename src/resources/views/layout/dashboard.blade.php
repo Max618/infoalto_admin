41e3569a0e0ad3bb94a84501ba19290dcf596e7d
Gabriel Santos <gabmaxsan@gmail.com>
2019-01-17 14:12:27 -0200

    display form errors

--- src/resources/views/layout/dashboard.blade.php
+++ src/resources/views/layout/dashboard.blade.php
@@ -41,6 +41,14 @@
 			<!-- MAIN CONTENT -->
 			<div class="main-content">
 				<div class="container-fluid">
+					@if($errors->any())
+						@foreach($errors->all() as $error)
+							<div class="alert alert-danger alert-dismissible" role="alert">
+								<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+								<i class="fa fa-times-circle"></i> {{ $error }}
+							</div>
+						@endforeach
+					@endif
                     @yield('content')
                 </div>
             </div>
