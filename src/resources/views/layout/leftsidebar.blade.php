356858209afcaf7d545e6f6892e42d93879b32b3
Gabriel Santos <gabmaxsan@gmail.com>
2019-01-18 14:58:48 -0200

    fix layout bugs

--- src/resources/views/layout/leftsidebar.blade.php
+++ src/resources/views/layout/leftsidebar.blade.php
@@ -9,16 +9,6 @@
                 @can('create_role')
                 <li><a href="{{ route('role.index') }}" ><i class="lnr lnr-graduation-hat"></i> <span>Funções</span></a></li>
                 @endcan
-                <li>
-                    <a href="#subPages" data-toggle="collapse" class="collapsed"><i class="lnr lnr-file-empty"></i> <span>Pages</span> <i class="icon-submenu lnr lnr-chevron-left"></i></a>
-                    <div id="subPages" class="collapse ">
-                        <ul class="nav">
-                            <li><a href="page-profile.html" class="">Profile</a></li>
-                            <li><a href="page-login.html" class="">Login</a></li>
-                            <li><a href="page-lockscreen.html" class="">Lockscreen</a></li>
-                        </ul>
-                    </div>
-                </li>
             </ul>
         </nav>
     </div>
