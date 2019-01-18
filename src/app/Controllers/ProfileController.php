6f267f3a2a7ada89f5600577418af3d85b2922ce
Gabriel Santos <gabmaxsan@gmail.com>
2019-01-18 14:52:01 -0200

    refactory store function

--- src/app/Controllers/ProfileController.php
+++ src/app/Controllers/ProfileController.php
@@ -40,20 +40,27 @@ class ProfileController extends Controller
     public function store(ProfileCreateRequest $request) {
         try{
             $user = auth()->user();
-            $directory_image = "storage/profiles/";
-            $name_image = $user->id."-profile.".$request->file("profile_image")->extension();
-
-            $path = $request->file('profile_image')->storeAs("profiles/", $name_image);
-
             $user->profile()->create($request->only('birthday','phone','about'));
-            $user->profile->image()->create([
-                "title" => "Imagem de perfil de $user->name",
-                "directory" => $directory_image,
-                "name" => $name_image
-            ]);
+            $this->imageProfileUpload($request);
             return redirect()->route("profile.index")->with("success","Informações atualizado com sucesso!");
         } catch(Exception $error){
             return redirect()->route("profile.index")->with("error",$error->getMessage());
         }
+    }
+
+    private function imageProfileUpload(ProfileCreateRequest $request){
+        $user = auth()->user();
+
+        $directory_image = "storage/profiles/";
+        $name_image = $user->id."-profile.".$request->file("profile_image")->extension();
+
+        if(!$request->file('profile_image')->storeAs("profiles/", $name_image))
+            throw new Exception("Erro ao fazer upload da imagem");
+
+        $user->profile->image()->create([
+            "title" => "Imagem de perfil de $user->name",
+            "directory" => $directory_image,
+            "name" => $name_image
+        ]);
     }
 }
