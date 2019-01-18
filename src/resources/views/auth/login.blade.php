50886b149a5d08513b81df0e2aad3d4ec938852d
Gabriel Santos <gabmaxsan@gmail.com>
2019-01-18 20:50:05 -0200

    translate view

--- src/resources/views/auth/login.blade.php
+++ src/resources/views/auth/login.blade.php
@@ -8,17 +8,17 @@
         <div class="content">
             <div class="header">
                 <div class="logo text-center"><img src="{{ asset('admin/img/logo-dark.png') }}" alt="Klorofil Logo"></div>
-                <p class="lead">Login to your account</p>
+                <p class="lead">Entre no sistema</p>
             </div>
             <form class="form-auth-small" method="POST" action="{{ route('login') }}">
             @csrf
                 <div class="form-group">
-                    <label for="signin-email" class="control-label sr-only">Email</label>
-                    <input type="email" class="form-control" id="signin-email" name="email" placeholder="Email">
+                    <label for="signin-email" class="control-label sr-only">E-mail</label>
+                    <input type="email" class="form-control" id="signin-email" name="email" placeholder="E-mail">
                 </div>
                 <div class="form-group">
-                    <label for="signin-password" class="control-label sr-only">Password</label>
-                    <input type="password" class="form-control" id="signin-password" name="password" placeholder="Password">
+                    <label for="signin-password" class="control-label sr-only">Senha</label>
+                    <input type="password" class="form-control" id="signin-password" name="password" placeholder="Senha">
                 </div>
                 <div class="form-group clearfix">
                     <label class="fancy-checkbox element-left">
@@ -26,9 +26,9 @@
                         <span>Remember me</span>
                     </label>
                 </div>
-                <button type="submit" class="btn btn-primary btn-lg btn-block">LOGIN</button>
+                <button type="submit" class="btn btn-primary btn-lg btn-block">ENTRAR</button>
                 <div class="bottom">
-                    <span class="helper-text"><i class="fa fa-lock"></i> <a href="{{ route('password.request') }}">Forgot password?</a></span>
+                    <span class="helper-text"><i class="fa fa-lock"></i> <a href="{{ route('password.request') }}">Esquecer sua senha?</a></span>
                 </div>
             </form>
         </div>
@@ -36,8 +36,8 @@
     <div class="right">
         <div class="overlay"></div>
         <div class="content text">
-            <h1 class="heading">Free Bootstrap dashboard template</h1>
-            <p>by The Develovers</p>
+            <h1 class="heading">Sistema gerenciador de conteúdo</h1>
+            <p>desenvolvido por <a target="_blank" href="https://www.infoalto.com.br">Infoalto</a></p>
         </div>
     </div>
     <div class="clearfix"></div>
--- src/resources/views/auth/passwords/email.blade.php
+++ src/resources/views/auth/passwords/email.blade.php
@@ -8,12 +8,12 @@
         <div class="content">
             <div class="header">
                 <div class="logo text-center"><img src="{{ asset('admin/img/logo-dark.png') }}" alt="Klorofil Logo"></div>
-                <p class="lead">Email para resetar a senha</p>
+                <p class="lead">Resetar a senha</p>
             </div>
             <form class="form-auth-small" action="index.php">
                 <div class="form-group">
-                    <label for="signin-email" class="control-label sr-only">Email</label>
-                    <input type="email" class="form-control" id="signin-email" value="samuel.gold@domain.com" placeholder="Email">
+                    <label for="signin-email" class="control-label sr-only">E-mail</label>
+                    <input type="email" class="form-control" id="signin-email" placeholder="E-mail">
                 </div>
                 <button type="submit" class="btn btn-primary btn-lg btn-block">ENVIAR</button>
             </form>
@@ -22,8 +22,8 @@
     <div class="right">
         <div class="overlay"></div>
         <div class="content text">
-            <h1 class="heading">Free Bootstrap dashboard template</h1>
-            <p>by The Develovers</p>
+            <h1 class="heading">Sistema gerenciador de conteúdo</h1>
+            <p>desenvolvido por <a target="_blank" href="https://www.infoalto.com.br">Infoalto</a></p>
         </div>
     </div>
     <div class="clearfix"></div>
