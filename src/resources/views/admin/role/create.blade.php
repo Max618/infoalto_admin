6a930709eafb5a6f9408b93d3c52997795efa6d5
Gabriel Santos <gabmaxsan@gmail.com>
2019-04-30 15:30:52 -0300

    WIP - try create helpers functions file

--- src/resources/views/admin/role/create.blade.php
+++ src/resources/views/admin/role/create.blade.php
@@ -3,6 +3,7 @@
 @section("title", "Criar Função")
 
 @section('content')
+{{dd(getModels())}}
 <h3 class="page-title">Criar Função</h3>
 <div class="row">
     <div class="col-md-12">
@@ -15,7 +16,12 @@
                 {!! Form::textarea("description",null, ["class" => "form-control", "placeholder" => "Digite a Descrição"]) !!}
                 <br>
                 {!! Form::text("permissions",null, ["class" => "form-control", "placeholder" => "Digite as permissões separdas por / usando o padrao (create,edit,view,delete) e a model"]) !!}
-                <br>
+                <div class="col-md-12">
+                    <div class="col-md-6">
+                        <input type="checkbox"/>Todas
+                    </div>
+                    <div class="col-md-6"></div>
+                </div>
                 <div class="col-md-12 text-center">
                     {!! Form::submit("Salvar", ["class" => "btn btn-primary"]) !!}
                     <a class="btn btn-danger" href="{{ route('role.index') }}">Cancelar</a>
